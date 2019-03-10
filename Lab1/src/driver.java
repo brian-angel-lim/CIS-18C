953640a0ca67cd090cfb39426aa3740895b94802
Angel Lim <brian.q.lim@gmail.com>
2019-03-09 23:56:27 -0800

    Started work on Driver

--- Lab1/src/driver.java
+++ Lab1/src/driver.java
@@ -1,5 +1,24 @@
-public class driver {
-    public static void main(String[] arg) {
+import java.util.Scanner;
 
+public class driver {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        shapeMenu();
+    }
+    
+    public static void shapeMenu()
+    {
+        System.out.println("Welcome to the Shape-O-Mattic! Please enter what shape you want to make: ");
+        System.out.println("==========================================================================");
+        //Quadrilaterals
+        System.out.println("A. Square");
+        System.out.println("B. Rectangle");
+        System.out.println("C. Pentagon");
+        System.out.println("D. Hexagon");
+        System.out.println("E. Octagon");
+        //Triangles
+        System.out.println("F. Equilateral Triangle");
+        System.out.println("B. Isosceles Triangle");
     }
 }
