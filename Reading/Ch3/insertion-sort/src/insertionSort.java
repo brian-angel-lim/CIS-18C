63499c722ef435e2f521969d5bcc7adbe7906cbc
Angel Lim <brian.q.lim@gmail.com>
2019-03-18 13:55:33 -0700

    started work on InsertionSort()

---
+++ Reading/Ch3/insertion-sort/src/insertionSort.java
@@ -0,0 +1,12 @@
+/*
+insertion sort
+>Algorithm proceeds by considering one element at a time, placing
+the element in the correct order relative to those before it
+>We start with the first element in the array, which sorts itself
+>For the next element, if it is smaller than the first, we swap them
+>For the third element, swap it leftword until it is in its proper order relative to the first two elements
+>Continue this method with the fourth, fifth, and so on, until whole array is sorted
+ */
+
+public class insertionSort {
+}
