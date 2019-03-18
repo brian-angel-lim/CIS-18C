6b5fa694ddc63a97e3ad746e2bb05f89ea1ed00a
Angel Lim <brian.q.lim@gmail.com>
2019-03-18 14:22:42 -0700

    Finished insertionSort()

--- Reading/Ch3/insertion-sort/src/insertionSort.java
+++ Reading/Ch3/insertion-sort/src/insertionSort.java
@@ -1,12 +1,18 @@
-/*
-insertion sort
->Algorithm proceeds by considering one element at a time, placing
-the element in the correct order relative to those before it
->We start with the first element in the array, which sorts itself
->For the next element, if it is smaller than the first, we swap them
->For the third element, swap it leftword until it is in its proper order relative to the first two elements
->Continue this method with the fourth, fifth, and so on, until whole array is sorted
- */
+//Insertion-sort of an array of characters into non-decreasing order
 
 public class insertionSort {
+    public static void insertionSort(char[] data) {
+        int n = data.length;
+
+        for (int k = 1; k < n;k++) {                //begin with second element
+            char cur = data[k];                     //cur = current element on data array, relative to for-loop index
+            int j = k;                              //int j = current index of for loop, j will be element that is manipulated
+            //find correct index j for cur
+            while (j > 0 && data[j-1] > cur) {      //thus, data[j-1] must go after cur
+                data[j] = data[j-1];                //slide data[j-1] rightward
+                j--;
+            }
+            data[j] = cur;                          //this is the proper place for cur
+        }
+    }
 }
