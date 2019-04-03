367a4f33398262ace097b8af8aa4b8e926441f59
Angel Lim <brian.q.lim@gmail.com>
2019-04-02 20:52:23 -0700

    Converted Integer data types of a generic <T>

--- Notes/Stacks/src/ArrayStack.java
+++ Notes/Stacks/src/ArrayStack.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
-public class ArrayStack implements IStack{
+public class ArrayStack<T> implements IStack<T>{
 
-    ArrayList<Integer> myData;
+    ArrayList<T> myData;
 
     public ArrayStack() {
-        myData = new ArrayList<Integer>();
+        myData = new ArrayList<T>();
     }
+
     @Override
-    public void push(Integer itemToPush) {
+    public void push(T itemToPush) {
         myData.add(itemToPush);
     }
 
     @Override
-    public Integer pop() {
+    public T pop() {
         return myData.remove(myData.size() - 1);
     }
 
--- Notes/Stacks/src/IStack.java
+++ Notes/Stacks/src/IStack.java
@@ -1,5 +1,7 @@
-public interface IStack {
-    public void push(Integer itemToPush);
-    public Integer pop();
+//we're creating T, which is a "generic"... Java's version of C++'s template
+
+public interface IStack<T> {
+    public void push(T itemToPush);
+    public T pop();
     public Integer size();
 }
