87a140f83cb67c65da5fac7a1b6c4066aed1b174
Angel Lim <brian.q.lim@gmail.com>
2019-05-28 17:25:02 -0700

    Final test

--- Library Reservation System/src/Student.java
+++ Library Reservation System/src/Student.java
@@ -1,4 +1,20 @@
-public class Student implements {
-    int ID = 0;
-    String Name = "";
+public class Student implements User{
+    private int ID = 0;
+    private String Name = "";
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
